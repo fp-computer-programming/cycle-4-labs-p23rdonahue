7264271af98e20f7b5ab4ff435d90976af116c5d
Robbie Donahue <p23rdonahue@fairfieldprep.org>
2022-10-11 10:25:45 -0400

    lab 4-1 complete

---
+++ lab_4-1.py
@@ -0,0 +1,11 @@
+# author: RED 10/11/22
+
+# getting first and last name and prompting input
+
+first_name = input("What is your first name?")
+last_name = input("What is your last name?")
+
+# combining name into full name and printing
+
+full_name = first_name + ' ' + last_name
+print(full_name)
