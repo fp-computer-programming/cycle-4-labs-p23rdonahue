4d8304e7527da92360a09d71972692e3ff31dc97
Robbie Donahue <p23rdonahue@fairfieldprep.org>
2022-10-13 11:21:03 -0400

    lab 4-2 doneski

---
+++ lab_4-2.py
@@ -0,0 +1,10 @@
+#author: RED 10/13/22
+#define variables
+school = 'Fairfield Prep'
+#splitting these bad boys
+first_half = school[:9]
+second_half = school[9:]
+#printing these bad boys
+print(first_half)
+print(second_half)
+print(first_half + second_half)
