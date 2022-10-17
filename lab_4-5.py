346cc3c73931da74adae61ace44ff1756d88e550
Robbie Donahue <p23rdonahue@fairfieldprep.org>
2022-10-17 13:16:54 -0400

    lab 4-5 complete

---
+++ lab_4-5.py
@@ -0,0 +1,11 @@
+#author: RED 10/17/22
+
+#input statements
+
+person1 = input("What is your name silly goose?")
+person2 = input("How about you my second friend?")
+
+#making sentence
+
+fs= "Hello, {0}. My name is {1}.".format(person1, person2)
+print(fs)
