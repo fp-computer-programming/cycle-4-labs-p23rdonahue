3268442c0404937a5e5a43d87da64a6347df60f0
Robbie Donahue <p23rdonahue@fairfieldprep.org>
2022-10-17 13:31:29 -0400

    4-4 completed

---
+++ lab_4-4.py
@@ -0,0 +1,13 @@
+#author: REd 10/17/22
+
+x = "fliggertigibit"
+first_name= 'Robbie'
+sentence= 'I wish, I wish, I was a fish'
+
+letter_t = x.find("t")
+
+print(x[letter_t + 1])
+
+print(first_name.upper())
+
+print(sentence.split(sep=",", maxsplit=-1))
